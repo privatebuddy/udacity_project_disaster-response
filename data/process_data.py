362b9dfa3a19b372833b10dc506b4e4ba5216cf9
sasin-mbp <privatebuddy@gmail.com>
2021-06-29 12:16:20 +0700

    Update: make sure data override to exist database when recreate ETL

--- data/process_data.py
+++ data/process_data.py
@@ -77,7 +77,7 @@ def save_data(df, database_filename):
     """
     sqlite_path = "sqlite:///{0}".format(database_filename)
     engine = create_engine(sqlite_path)
-    df.to_sql('message', engine, index=False)
+    df.to_sql('message', engine, index=False, if_exists='replace')
 
 
 def main():
